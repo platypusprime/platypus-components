6e7f28ca03cd72d2569912c95c13911594de5bc4
Jingchen Xu <joel.jc.xu@gmail.com>
2016-06-06 21:55:37 -0400

    Convert project to Maven project

---
+++ Platypus Components/src/test/java/Tester.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import platypus.components.text.PDateChooser;
+
+public class Tester {
+
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new PDateChooser());
+		frame.setVisible(true);
+		frame.pack();
+	}
+
+}
