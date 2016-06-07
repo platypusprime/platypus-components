2787b52248bdb6c0363145c2c9ee06019e8c20f9
Jingchen Xu <joel.jc.xu@gmail.com>
2016-06-06 22:19:13 -0400

    Make minor fixes for Maven conversion

--- Platypus Components/src/test/java/platypus/components/Tester.java
+++ Platypus Components/src/test/java/platypus/components/Tester.java
@@ -1,3 +1,5 @@
+package platypus.components;
+
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -6,9 +8,8 @@ import platypus.components.text.PDateChooser;
 
 public class Tester {
 
-	public static void main(String[] args) throws ClassNotFoundException,
-			InstantiationException, IllegalAccessException,
-			UnsupportedLookAndFeelException {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
 		JFrame frame = new JFrame();
